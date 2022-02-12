41ae6de4ffdd2b1edb29eacfe3e4616030c61f0c
Fatih Gucuko <fatih.gucuko@webinify.com>
2022-02-12 15:42:00 +0100

    adding single file template install

---
+++ installer/apache-template-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source common/functions.sh;
+
+update_template_dir apache2 https://gitlab.hyadescloud.com/hyadescloud/vesta-apache-template.git
---
+++ installer/nginx-template-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+source common/functions.sh;
+
+
+update_template_dir nginx https://gitlab.hyadescloud.com/hyadescloud/vesta-nginx-templates.git
