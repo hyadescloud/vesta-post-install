cc2ced394149026a8dd870b7cfb193a415588fb8
Fatih Gucuko <fatih.gucuko@webinify.com>
2022-02-12 20:23:43 +0100

    adding cron jobs auto

--- cron/add-crons.sh
+++ cron/add-crons.sh
@@ -4,6 +4,6 @@ source common/functions.sh;
 
 echo "Adding cron tab to auto restart service on crash"
 
-add_cron "* * * * * ~/vesta-post-install/cron/vesta_service.sh"
+add_cron "* * * * * /root/vesta-post-install/cron/vesta_service.sh"
 
 
