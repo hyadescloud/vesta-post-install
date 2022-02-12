2a2af9e41a57b5faec018de875759b3eadb469aa
Fatih Gucuko <fatih.gucuko@webinify.com>
2022-02-12 20:28:42 +0100

    update

--- post-install-debian-9.sh
+++ post-install-debian-9.sh
@@ -7,10 +7,7 @@ apt-get update -y
 apt-get upgrade -y
 
 echo "Installing necessary packages"
-apt-get install git zip -y
-
-apt-get update
-apt install -y apt-transport-https ca-certificates
+apt-get install -y git zip apt-transport-https ca-certificates
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 sh -c 'echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list'
 
@@ -23,3 +20,6 @@ bash installer/php-install.sh
 echo "Setting up new templates"
 update_template_dir apache2 https://gitlab.hyadescloud.com/hyadescloud/vesta-apache-template.git
 update_template_dir nginx https://gitlab.hyadescloud.com/hyadescloud/vesta-nginx-templates.git
+
+echo "Adding cron jobs"
+bash cron/add-crons.sh
--- post-install-ubuntu.sh
+++ post-install-ubuntu.sh
@@ -8,10 +8,7 @@ apt-get update -y
 apt-get upgrade -y
 
 echo "Installing necessary packages"
-apt-get install git zip -y
-
-apt-get update -y
-apt install -y apt-transport-https ca-certificates
+apt-get install -y git zip apt-transport-https ca-certificates
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 
 apt-get update
@@ -21,6 +18,8 @@ echo "Installing necessary packages"
 bash installer/php-install.sh
 
 echo "Setting up new templates"
-
 update_template_dir apache2 https://gitlab.hyadescloud.com/hyadescloud/vesta-apache-template.git
 update_template_dir nginx https://gitlab.hyadescloud.com/hyadescloud/vesta-nginx-templates.git
+
+echo "Adding cron jobs"
+bash cron/add-crons.sh
